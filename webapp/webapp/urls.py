0db98091c6596c8f6f5112b814e3a9e529a50e18
Aleksander Kopeć <aleksander.kopec@relayr.io>
2022-03-23 16:54:04 +0100

    [WEBAPP] Minor changes and requirements.txt

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+    path('', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
