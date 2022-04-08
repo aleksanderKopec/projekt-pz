623b5d3c95dd479d8a2d7e4f4dfc67aca2932995
Aleksander Kopeć <a.kopec.b@gmail.com>
2022-04-08 21:21:24 +0200

    [WEBAPP] Fix settings.py

--- webapp/chat/settings.py
+++ webapp/chat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wmsb2m$x9eak06g2xc0*-0w*nemb)#*qw2!$=4g7f8g-j%z_81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.130']
 
 
 # Application definition
