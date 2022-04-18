3895a62963144dbe664c4aba898363dd17cdc68c
Wojtek Kopanski <wilq.xp@gmail.com>
2022-04-19 00:10:11 +0200

    refactor api

--- api/app/conn_managers.py
+++ api/app/conn_managers.py
@@ -9,45 +9,62 @@ from typing import Any, Dict, Union, List, Tuple
 
 from pymongo.collection import Collection
 
+from model.model import ChannelIdModel, MessageModel
+
+
+def _validate_b64(s: str) -> bool:
+    try:
+        base64.b64decode(s, validate=True)
+    except binascii.Error:
+        return False
+    return True
+
 
 class DBManager:
-    def __init__(self, mongo_url):
+    def __init__(self, mongo_url) -> None:
         self.db_client = pymongo.MongoClient(mongo_url)
 
         self.db = self.db_client["chat_db"]
-        self.channel_id_db = self.db["channel_id"]
+        self.channels = self.db["channels"]
 
-        self.db["room_0"]
-        self.channel_id_db.insert_one({"channel_id": "0", "channel_no": 0})
+        # if there are no channels create one virtually
+        if not list(self.channels.find()):
+            self.channels.insert_one(ChannelIdModel(
+                channel_no=0, channel_id="MAo=").dict())
 
-    async def get_channel(self, channel_id: str):
-        # validate base64
-        try:
-            base64.b64decode(channel_id)
-        except binascii.Error:
-            raise HTTPException(
-                status_code=500, detail=f"Channel ID not b64 encoded")
+    def get_channel_no(self, channel_id: str) -> int:
+        if not _validate_b64(channel_id):
+            raise Exception("Channel id has to be a valid base64 string.")
 
-        channel_id_no = self.channel_id_db.find_one({"channel_id": channel_id})
-        if channel_id_no is None:
-            channel_no = self.channel_id_db.find_one(
-                sort=[("channel_id", pymongo.DESCENDING)])["channel_no"] + 1
-            channel_id_no = {"channel_id": channel_id,
-                             "channel_no": channel_no}
-            self.channel_id_db.insert_one(channel_id_no)
-
-        channel_no = channel_id_no["channel_no"]
-        channel = self.db[f"room_{channel_no}"]
-        print(channel_no)
-        return (channel, channel_no)
-
-    async def get_last_message_no(self, channel_no):
-        last_msg = self.db[f"room_{channel_no}"].find_one(sort=[("message_id", pymongo.DESCENDING)])
-        if last_msg is None:
-            last_msg = 0
+        channel = self.channels.find_one({"channel_id": channel_id})
+        if channel is not None:
+            channel_no = channel["channel_no"]
         else:
-            last_msg = last_msg["message_id"]
-        return last_msg
+            channel_no = self.channels.find_one(
+                sort=[("channel_no", pymongo.DESCENDING)])["channel_no"] + 1
+            self.channels.insert_one(ChannelIdModel(
+                channel_no=channel_no, channel_id=channel_id).dict())
+
+        return channel_no
+
+    def get_channel(self, channel_id: str) -> Collection:
+        return self.db[f"channel_{self.get_channel_no(channel_id)}"]
+
+    def get_messages(self, channel: Collection, number_of_messages: int = 0) -> list:
+        messages = channel.find(
+            sort=[("message_no", pymongo.DESCENDING)], limit=number_of_messages)
+        return list(messages)
+
+    def is_channel_empty(self, channel: Collection) -> bool:
+        if not list(channel.find()):
+            return False
+        return True
+
+    def get_next_message_no(self, channel: Collection) -> int:
+        message = channel.find_one(sort=[("message_no", pymongo.DESCENDING)])
+        if message is None:
+            return 1
+        return message["message_no"] + 1
 
 
 class ConnectionManager:
@@ -66,19 +83,15 @@ class ConnectionManager:
             await connection.send_text(message)
 
 
-class ChannelManager:
-    def __init__(self, db):
-        self.db = db
-        self.channels: Dict[int: ConnectionManager] = {}
+class ChannelConnectionManager:
+    def __init__(self, db_manager: DBManager):
+        self.db_manager = db_manager
+        self.channel_connections: Dict[int: ConnectionManager] = {}
 
-    def _open_channel(self, channel_no):
-        channel = ConnectionManager()
-        self.channels[channel_no] = channel
-        return channel
-
-    def get_channel(self, channel_no):
+    def get_channel_connection(self, channel_id: str) -> ConnectionManager:
+        channel_no = self.db_manager.get_channel_no(channel_id)
         try:
-            channel = self.channels[channel_no]
+            channel_connection = self.channel_connections[channel_no]
         except KeyError:
-            channel = self._open_channel(channel_no)
-        return channel
+            channel_connection = ConnectionManager()
+        return channel_connection
--- api/app/model/model.py
+++ api/app/model/model.py
@@ -1,21 +1,59 @@
-import datetime
 from typing import List
 
 from pydantic import BaseModel
 
 
 class MessageModel(BaseModel):
-    message_id: int
+    message_no: int
     author: str
     message: str
-    timestamp: datetime.datetime
+    timestamp: str  # datetime.datetime.isoformat('2022-04-18T18:53:22.831797')
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "message_no": 1,
+                "author": "Jane Doe",
+                "message": "Hi!",
+                "timestamp": "2022-04-18T18:53:22.831797",
+            }
+        }
 
 
-class ChannelModel(BaseModel):
+class MessagesModel(BaseModel):
     channel_id: str
     messages: List[MessageModel]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "channel_id": "dGVzdAo=",
+                "messages": [
+                    {
+                        "message_no": 1,
+                        "author": "Jane Doe",
+                        "message": "Hi!",
+                        "timestamp": "2022-04-18T18:53:22.831797",
+                    },
+                    {
+                        "message_no": 2,
+                        "author": "John Smith",
+                        "message": "Hello!",
+                        "timestamp": "2022-04-18T18:54:32.832345",
+                    }
+                ]
+            }
+        }
 
 
 class ChannelIdModel(BaseModel):
     channel_no: int
     channel_id: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "channel_no": 1,
+                "channel_id": "dGVzdAo="
+            }
+        }
