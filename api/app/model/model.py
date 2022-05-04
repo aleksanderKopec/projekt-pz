9ef185711cfb6598e7727b79f3da4f30abdaad54
Wojtek Kopanski <wilq.xp@gmail.com>
2022-05-04 20:00:54 +0200

    add is_encrypted field and switch to json message

--- api/app/model/model.py
+++ api/app/model/model.py
@@ -8,6 +8,7 @@ class MessageModel(BaseModel):
     author: str
     message: str
     timestamp: str  # datetime.datetime.isoformat('2022-04-18T18:53:22.831797')
+    is_encrypted: bool
 
     class Config:
         schema_extra = {
@@ -16,6 +17,7 @@ class MessageModel(BaseModel):
                 "author": "Jane Doe",
                 "message": "Hi!",
                 "timestamp": "2022-04-18T18:53:22.831797",
+                "is_encrypted": False
             }
         }
 
@@ -34,12 +36,14 @@ class MessagesModel(BaseModel):
                         "author": "Jane Doe",
                         "message": "Hi!",
                         "timestamp": "2022-04-18T18:53:22.831797",
+                        "is_encrypted": False
                     },
                     {
                         "message_no": 2,
                         "author": "John Smith",
                         "message": "Hello!",
                         "timestamp": "2022-04-18T18:54:32.832345",
+                        "is_encrypted": False
                     }
                 ]
             }
